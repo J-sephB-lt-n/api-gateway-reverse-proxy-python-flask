f82cba9d1822830de3efda4d4ecb0cfc626de5b0
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2023-11-05 21:35:40 +0200

    fix(endpoint_routing.py): changed "https://httpbin.org/post" to "https://httpbin.org/get"

--- endpoint_routing.py
+++ endpoint_routing.py
@@ -1,6 +1,6 @@
 """docstring TODO"""
 
 endpoint_routing: dict[str, str] = {
-    "test_get_request/v1": "https://httpbin.org/post",
+    "test_get_request/v1": "https://httpbin.org/get",
     "test_post_request/v1": "https://httpbin.org/post",
 }
